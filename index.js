2d52b6b3817af33976269dd3e4f8e7e109af8028
Gabriel <gcs232005@gmail.com>
2024-09-10 16:56:00 -0300

    at3

--- index.js
+++ index.js
@@ -1,6 +1,46 @@
-const {select} = require(`@inquirer/prompts`)
+const {select, input, checkbox} = require(`@inquirer/prompts`)
+let metas = []
+
+const cadastrarMeta = async () => {
+    const meta = await input({ message: "Digite a Meta: "})
+
+    if (meta.length == 0) {
+        console.log("A Meta Não Pode Ser Vazia")
+        return
+    }
+
+    metas.push({value: meta, checked: false})
+}
+
+const listarMeta = async () => {
+    const resposta = await checkbox ({
+        message: "Use as Setas Para Mudar de Meta, o Espaço Para Marcar e Descarcar e o Enter Para Finalizar Essa Etapa",
+        choices: [...metas],
+        instructions: false,                 
+    })
+
+    if (resposta.length == 0) {
+        console.log("Nenhuma Meta Selecionada !!!")
+        return
+    }
+
+    metas.forEach((m) => {
+        m.checked = false
+    })
+
+    resposta.forEach((r) => {
+        const meta = metas.find((m) => {
+            return m.value == resposta
+        })
+
+        meta.checked = true
+    })
+
+}
 
 const start = async () => {
+
+
     while (true) {
         const opc = await select({
             message: "Menu >",
@@ -23,10 +63,11 @@ const start = async () => {
 
         switch (opc) {
             case "cadastrar":
-                console.log("vamos cadastrar")
+                await cadastrarMeta()
+                console.log(metas)
                 break
             case "listar":
-                console.log("vamos listar")
+                await listarMeta()
                 break
             case "sair":
                 return
